e292acbec3451cc34b071ed3c6ed0923d450b9c0
Iván Iguarán <ivan.iguaran@ideaware.co>
2018-05-17 16:55:06 -0500

    Add security group to the stack
    Apply security group to the instance
    
    Use interpolation variables

--- main.tf
+++ main.tf
@@ -5,8 +5,26 @@ provider "aws" {
 resource "aws_instance" "sample" {
   ami = "ami-43a15f3e"
   instance_type = "t2.micro"
-  
+  vpc_security_group_ids = ["${aws_security_group.sample.id}"]
+
+  user_data = <<-EOF
+  #!/bin/bash
+  echo "Hello Ideaware!" > index.html
+  nohup busybox httpd -f -p 8080 &
+  EOF
+
   tags {
     Name = "sample-terraform"
+  }
+}
+
+resource "aws_security_group" "sample" {
+  name = "terraform-sample-security-group"
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
